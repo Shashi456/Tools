419ba238894f4bf25b782ce0fd3d9506830553c4
PAWAN SASANKA AMMANAMANCHI <pass2pawan@gmail.com>
2018-05-25 16:07:34 +0530

    LearnPythonTheHardWay exercises

--- python/LearnPythonTheHardWay.py
+++ python/LearnPythonTheHardWay.py
@@ -3,6 +3,7 @@
 
 
 # String formatting
+# Strings can be printed with help of escape sequences as well
 a = 40
 # the following f is for formatting the strings
 # the {} -> is for mentioning the variable
@@ -11,18 +12,18 @@ b = 50
 #Two ways of formating, the .format is more readable
 print(f"How do you know {a} + {b} is 90?")
 print("How do you know {} + {} is 90?".format(a,b))
+print("How do you know %i + %i is 90?" %(a,b))
+# the parameters after % needs to be in a tuple
 
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
-print(end1 + end2 + end3 + end4 + end5 + end6)#, end=' ')
-print(end7 + end8 + end9 + end10 + end11 + end12)
+
+# end is the delimiter, default newline , can be changed
+# but changing it always can be cumbersome
+print("Good", end = ' ')
+print("Morning")
+
+
+from sys import argv
+f,s,t = argv
+print(f)
+print(s)
+print(t)
